3a75a74c42cb404b28a121d53bb5fa8625f2209f
AdityaVishwekar <avishwekar10893@gmail.com>
2017-02-04 08:51:54 -0800

    Reverse-String & Factorial

---
+++ javascript_Functions/convertCelsiusToFahrenheit.js
@@ -0,0 +1,13 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js">
+$("document").ready(
+	function convertToF(celsius) {
+  		var fahrenheit;
+  		// Only change code below this line
+  		fahrenheit = (celsius*9/5)+32;
+  
+  		// Only change code above this line
+  		window.alert(fahrenheit);
+	}
+convertToF(30);
+);
+</script>
